63add79759a590eebb1051f52547c73baad1493e
palashichevaa@mail.ru <palashichevaa@mail.ru>
2022-11-14 13:03:26 +1000

    Добавьте файлы проекта.

---
+++ ApplicationData/AppConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISRPO_Palashicheva_PR12.ApplicationData
+{
+    class AppConnect
+    {
+        public static FirstEntities modelOdb;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ISRPO_Palashicheva_PR12.ApplicationData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISRPO_Palashicheva_PR12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppConnect.modelOdb = new FirstEntities(); // подключение к БД
+            AppFrame.frameMain = FrmMain; //загрузка фрейма с началом работы программы
+        }
+    }
+}
